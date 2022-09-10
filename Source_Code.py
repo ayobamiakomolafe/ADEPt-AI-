b5715b5a6ab4c27a3582e83a233df81454c0afe5
Ayobami <68803580+ayobamiakomolafe@users.noreply.github.com>
2022-09-10 22:26:25 +0100

    Update Source_Code.py

--- Source_Code.py
+++ Source_Code.py
@@ -134,105 +134,97 @@ import pandas as pd
 import plotly.express as px
 
 
-def main():
-    st.set_page_config(page_title='ADEPt-AI (Adverse Drug Effect Predictor - AI)',layout="wide", page_icon='Dependencies/RA.png')
-    
-    st.markdown("<h1 style='text-align: center; color: black;'>ADEPt-AI (Adverse Drug Effect Predictor - AI)</h1>", unsafe_allow_html=True)
-    st.markdown('##### By applying a machine learning approach to Rheumatoid Arthritis therapy, \
-     ADEPt-AI (Adverse Drug Effect Predictor - Artificial Intelligence) aims to improve \
-     prescription outcomes of Rheumatoid Arthritis drugs by predicting how individual \
-     patients will respond to medication based on demographic factors.')
-    st.write('\n')
-    st.markdown('##### ADEPt-AI iterates over possible combinations of \
-    adverse drug effects for every patient to predict potential drug interactions. Predictions are made automatically, in real-time, and at a low cost. \
-    This improves prescribing efficiency, reduces the time for clinical decisions,\
-    and potentially helps to improve the measurement of quality-adjusted life years(QUALYs).')
-    img=Image.open('Dependencies/RA.png')
 
-    cont = st.container()
-    with cont:
-        st.write('\n')
-        st.write('\n')
-        img=img.resize((700, 400))
-        st.image(img, use_column_width=True)
-    st.write('\n')
-    st.write('\n')
-        
+st.set_page_config(page_title='ADEPt-AI (Adverse Drug Effect Predictor - AI)',layout="wide", page_icon='Dependencies/RA.png')
 
-     # Collecting users Input   
-    country=st.sidebar.selectbox('Country', sorted(list(country_dict.keys())))
-    country=country_dict[country]
-    st.write('\n')
-    drug=st.sidebar.selectbox('Drug Name', drugs)
-    st.write('\n')
-    gender=st.sidebar.radio('Gender', list(sex_dict.keys()))
-    gender=sex_dict[gender]
-    st.write('\n')
-    age=st.sidebar.number_input('Age',min_value=1, max_value=100, step=1)
-    st.write('\n')
-    route=st.sidebar.selectbox('Drug Administration Route', sorted(list(adm_routes_dict.keys())))
-    route=adm_routes_dict[route]
-    st.write('\n')
-    drug_form=st.sidebar.selectbox('Drug Form', sorted(list(drug_form_dict.keys()) ))
-    drug_form=drug_form_dict[drug_form]
-    st.write('\n')
+st.markdown("<h1 style='text-align: center; color: black;'>ADEPt-AI (Adverse Drug Effect Predictor - AI)</h1>", unsafe_allow_html=True)
+st.markdown('##### By applying a machine learning approach to Rheumatoid Arthritis therapy, \
+ADEPt-AI (Adverse Drug Effect Predictor - Artificial Intelligence) aims to improve \
+prescription outcomes of Rheumatoid Arthritis drugs by predicting how individual \
+patients will respond to medication based on demographic factors.')
+st.write('\n')
+st.markdown('##### ADEPt-AI iterates over possible combinations of \
+adverse drug effects for every patient to predict potential drug interactions. Predictions are made automatically, in real-time, and at a low cost. \
+This improves prescribing efficiency, reduces the time for clinical decisions,\
+and potentially helps to improve the measurement of quality-adjusted life years(QUALYs).')
+img=Image.open('Dependencies/RA.png')
 
-    button=st.sidebar.button('Predict Reactions')
-
-    #Loading the encoder pickle files
-    pkl_file = open('Dependencies\Country.pkl', 'rb')
-    Country= pickle.load(pkl_file) 
-    pkl_file.close()
-
-    pkl_file = open('Dependencies\Drug.pkl', 'rb')
-    Drug= pickle.load(pkl_file) 
-    pkl_file.close()
-
-    pkl_file = open('Dependencies\Drug_Form.pkl', 'rb')
-    Drug_Form= pickle.load(pkl_file) 
-    pkl_file.close()
-
-    pkl_file = open('Dependencies\Route.pkl', 'rb')
-    Route_of_Administration= pickle.load(pkl_file) 
-    pkl_file.close()
-    
-    #Encoding users input for machine learning prediction
-    country=Country.transform([country])[0]
-    drug=Drug.transform([drug])[0]
-    drug_form=Drug_Form.transform([drug_form])[0]
-    route_of_administration=Route_of_Administration.transform([route])[0]
-    Data=[country,age,gender,drug,drug_form,route_of_administration]
-
-    
-    # Making a Prediction from users Input and Outputing the Result
-    if button:
-        st.markdown("<h1 style='text-align: center; color: black;'>Prediction Result</h1>", unsafe_allow_html=True)
-        model= CatBoostClassifier(verbose=0)
-        Predictions={}
-        for i in ['infection','pain', 'swelling', 'vomiting', 'headache', 'rash', 'nausea', 'pneumonia']:
-            path='Dependencies/' + i
-            model.load_model(path)
-            prediction=model.predict_proba(Data)
-            Predictions[i]= prediction
-        df=pd.DataFrame(Predictions).T.reset_index()
-        df[1]= df[1]*100
-        df.sort_values(1, inplace=True)
-        df.rename(columns={'index': 'ADVERSE EFFECTS', 1: 'PERCENTAGE PROBABILITY'}, inplace=True)
-        df['ADVERSE EFFECTS']=df['ADVERSE EFFECTS'].str.upper()
-        fig = px.bar(df, y='ADVERSE EFFECTS', x='PERCENTAGE PROBABILITY',height=800, orientation='h')
-        fig.update_layout(font_color="black", font_size=20)
-        st.plotly_chart(fig, use_container_width=True)
+cont = st.container()
+with cont:
+ st.write('\n')
+ st.write('\n')
+ img=img.resize((700, 400))
+ st.image(img, use_column_width=True)
+st.write('\n')
+st.write('\n')
 
 
-import streamlit 
-import sys
-from streamlit import cli as stcli
-if __name__ == '__main__':
-    if streamlit._is_running_with_streamlit:
-        main()
-    else:
-        sys.argv = ["streamlit", "run", sys.argv[0]]
-        sys.exit(stcli.main())
+# Collecting users Input   
+country=st.sidebar.selectbox('Country', sorted(list(country_dict.keys())))
+country=country_dict[country]
+st.write('\n')
+drug=st.sidebar.selectbox('Drug Name', drugs)
+st.write('\n')
+gender=st.sidebar.radio('Gender', list(sex_dict.keys()))
+gender=sex_dict[gender]
+st.write('\n')
+age=st.sidebar.number_input('Age',min_value=1, max_value=100, step=1)
+st.write('\n')
+route=st.sidebar.selectbox('Drug Administration Route', sorted(list(adm_routes_dict.keys())))
+route=adm_routes_dict[route]
+st.write('\n')
+drug_form=st.sidebar.selectbox('Drug Form', sorted(list(drug_form_dict.keys()) ))
+drug_form=drug_form_dict[drug_form]
+st.write('\n')
+
+button=st.sidebar.button('Predict Reactions')
+
+#Loading the encoder pickle files
+pkl_file = open('Dependencies\Country.pkl', 'rb')
+Country= pickle.load(pkl_file) 
+pkl_file.close()
+
+pkl_file = open('Dependencies\Drug.pkl', 'rb')
+Drug= pickle.load(pkl_file) 
+pkl_file.close()
+
+pkl_file = open('Dependencies\Drug_Form.pkl', 'rb')
+Drug_Form= pickle.load(pkl_file) 
+pkl_file.close()
+
+pkl_file = open('Dependencies\Route.pkl', 'rb')
+Route_of_Administration= pickle.load(pkl_file) 
+pkl_file.close()
+
+#Encoding users input for machine learning prediction
+country=Country.transform([country])[0]
+drug=Drug.transform([drug])[0]
+drug_form=Drug_Form.transform([drug_form])[0]
+route_of_administration=Route_of_Administration.transform([route])[0]
+Data=[country,age,gender,drug,drug_form,route_of_administration]
+
+
+# Making a Prediction from users Input and Outputing the Result
+if button:
+ st.markdown("<h1 style='text-align: center; color: black;'>Prediction Result</h1>", unsafe_allow_html=True)
+ model= CatBoostClassifier(verbose=0)
+ Predictions={}
+ for i in ['infection','pain', 'swelling', 'vomiting', 'headache', 'rash', 'nausea', 'pneumonia']:
+     path='Dependencies/' + i
+     model.load_model(path)
+     prediction=model.predict_proba(Data)
+     Predictions[i]= prediction
+ df=pd.DataFrame(Predictions).T.reset_index()
+ df[1]= df[1]*100
+ df.sort_values(1, inplace=True)
+ df.rename(columns={'index': 'ADVERSE EFFECTS', 1: 'PERCENTAGE PROBABILITY'}, inplace=True)
+ df['ADVERSE EFFECTS']=df['ADVERSE EFFECTS'].str.upper()
+ fig = px.bar(df, y='ADVERSE EFFECTS', x='PERCENTAGE PROBABILITY',height=800, orientation='h')
+ fig.update_layout(font_color="black", font_size=20)
+ st.plotly_chart(fig, use_container_width=True)
+
+
+
    
     
     
